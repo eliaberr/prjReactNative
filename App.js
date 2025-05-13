4946953325f99616903e0dd7a8f769b0902fea79
Eliabr Rodrigues <eliabe859@gmail.com>
2025-05-13 16:01:07 -0300

    finalizado

--- App.js
+++ App.js
@@ -3,20 +3,28 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   const [number, setNumber] = useState("");
-  const [numberConvertBinary, setNumberConvertBinary] = useState("")
+  const [numberConvertBinary, setNumberConvertBinary] = useState("");
+  const [showTable, setShowTable] = useState(false);
 
   const convertDecimalToBinari = () => {
-    let resultNumber = number
-    let binary = ''
+    let resultNumber = number;
+    let binary = "";
 
     do {
       const sobraNumber = resultNumber % 2;
-      binary = sobraNumber + binary
-      resultNumber = Math.floor(resultNumber / 2)
+      binary = sobraNumber + binary;
+      resultNumber = Math.floor(resultNumber / 2);
     } while (resultNumber > 0);
 
-    setNumberConvertBinary(binary)
-  }
+    setNumberConvertBinary(binary);
+  };
+
+  const convertBinariToDecimal = () => {
+    const binary = number;
+    const resultNumber = parseInt(binary, 2);
+    setNumberConvertBinary(resultNumber);
+    setShowTable(true)
+  };
 
   return (
     <View style={styles.container}>
@@ -27,25 +35,78 @@ export default function App() {
         onChangeText={setNumber}
         keyboardType="numeric"
       />
+      <Text
+        style={{
+          color: "white",
+          fontSize: 10,
+          width: 250,
+          textAlign: "center",
+        }}
+      >
+        Para converter binário para decimal, digite três ou mais caracteres.
+      </Text>
       <View style={styles.containerBtn}>
         <Button
           title="Converter para Binário"
           color="#841584"
           onPress={convertDecimalToBinari}
+          disabled={number.length > 2 ? true : false}
         />
+
         <Button
           title="Converter para Decimal"
           color="#841584"
-          onPress={() => alert("ai que dor!!!")}
+          onPress={convertBinariToDecimal}
+          disabled={number.length > 2 ? false : true}
         />
       </View>
       <View style={styles.result}>
-        <Text style={styles.resultText}>Resultado Decimal Para Binario</Text>
-        <TextInput
-          value={numberConvertBinary}
-          editable={false}
-          style={{color: 'black', textAlign:'center', backgroundColor:'#f1f1f1', borderRadius: 10, marginTop: 30}}
-        />
+        {number.length <= 2 ? (
+          <View>
+            <Text style={styles.resultText}>
+              Resultado Decimal Para Binario
+            </Text>
+            <TextInput
+              value={numberConvertBinary}
+              editable={false}
+              style={{
+                color: "black",
+                textAlign: "center",
+                backgroundColor: "#f1f1f1",
+                borderRadius: 10,
+                marginTop: 30,
+              }}
+            />
+          </View>
+        ) : (
+          <View>
+            <Text style={styles.resultText}>
+              Resultado Binario Para Decimal: {numberConvertBinary}
+            </Text>
+            {showTable ? (
+              <View style={styles.table}>
+                <View style={styles.row}>
+                  {number.split("").map((_, index) => {
+                    const length = number.length;
+                    const peso = Math.pow(2, length - index - 1);
+                    return (
+                      <Text key={index} style={styles.colum}>
+                        {peso}
+                      </Text>
+                    );
+                  })}
+                </View>
+                <View style={styles.row}>
+                  {number.split("").map((item, index) => {
+                    return <Text key={index}>{item}</Text>;
+                  })}
+                </View>
+              </View>
+            ) : (
+              <></>
+            )}
+          </View>
+        )}
       </View>
     </View>
   );
@@ -56,18 +117,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#f24533",
     alignItems: "center",
-    paddingTop: 100
+    paddingTop: 100,
+  },
+
+  table: {
+    backgroundColor: "white",
+    height: 70,
+    flexDirection: "colum",
+    justifyContent: "space-around",
+  },
+
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+
+  colum: {
+    textAlign: "center",
+    width: 25,
+    border: 1,
   },
 
   text: {
     color: "white",
-    fontSize: 30
+    fontSize: 30,
   },
 
   resultText: {
     color: "white",
-    fontSize: 25,
-    marginTop: 50
+    fontSize: 23,
+    marginTop: 50,
   },
 
   input: {
@@ -78,16 +157,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     backgroundColor: "white",
   },
-  
+
   containerBtn: {
-    grid:1,
+    grid: 1,
     gap: 50,
-    marginTop: 50
+    marginTop: 50,
   },
 
   result: {
     marginTop: 30,
-    justifyContent: 'center'
-    
-  }
+    justifyContent: "center",
+  },
 });
